4ab1e672dcffddc4d77fcaf6b42335311798aa01
Selina Shi <sshi26@live.unc.edu>
2022-03-01 19:26:16 -0500

    Changes to null argument case

--- flips.js
+++ flips.js
@@ -1,11 +1,13 @@
 import {coinFlips, countFlips } from "./modules/coin.mjs";
 const myArgs = process.argv.slice(2);
 
-const inputNumber = parseInt(myArgs[0].substring(9)); 
+let inputNumber; 
 
-if (inputNumber== null) {
-    inputNumber = 1; 
-}
+console.log(myArgs)
+console.log(process.argv)
+inputNumber = myArgs.length != 0 ? parseInt(myArgs[0].substring(9)) : 1;
+
+
 const results = coinFlips(inputNumber); 
 const counts = countFlips(results); 
 console.log(results); 
--- guess-flip.js
+++ guess-flip.js
@@ -1,9 +1,10 @@
 import {flipACoin, countFlips } from "./modules/coin.mjs";
 const myArgs = process.argv.slice(2);
 
-const inputString = myArgs[0].substring(7); 
+const inputString = (myArgs.length == 0 || !['heads', 'tails'].includes(myArgs[0].substring(7))) ? null : myArgs[0].substring(7);
 
-if (inputString != "heads" && inputString != 'tails') {
+
+if (inputString == null) {
     console.log("Error: no input."); 
     console.log("Usage: node guess-flip --call=[heads|tails]");
     process.exit();
