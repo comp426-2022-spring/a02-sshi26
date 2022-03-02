a6eb65f1d25869282081377ed52e03b0c4ab7fc1
Selina Shi <sshi26@live.unc.edu>
2022-03-01 19:40:36 -0500

    Changes to print statement

--- flips.js
+++ flips.js
@@ -3,8 +3,6 @@ const myArgs = process.argv.slice(2);
 
 let inputNumber; 
 
-console.log(myArgs)
-console.log(process.argv)
 inputNumber = (myArgs.length == 0 || myArgs[0].length <= 9) ? 1 : parseInt(myArgs[0].substring(9));
 
 
